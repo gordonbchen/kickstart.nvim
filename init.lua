05b201041f06f5923484331a1e2d89141a0f6087
gordonbchen <gordonbchen@gmail.com>
2025-04-26 01:03:02 -0400

    add tabbing settings

--- init.lua
+++ init.lua
@@ -104,6 +104,13 @@ vim.opt.number = true
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
+-- Tabbing.
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
